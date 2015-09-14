a43a2e0aa7dd01f4144ca9b9a290231f6f5f027a
exp3rt1 <olivier.proulx.1@ens.etsmtl.ca>
2015-09-14 11:50:57 -0400

    creation des methodes de décompression

--- LOG320-LAB1-HUFFMAN/src/huffman/Decompression.java
+++ LOG320-LAB1-HUFFMAN/src/huffman/Decompression.java
@@ -1,5 +1,16 @@
 package huffman;
 
-public class Decompression {
+public class Decompression 
+{
+	
+	public static void creationArbre()
+	{
+		System.out.print("creationArbre");
+	}
+	
+	public static void insertionTexte()
+	{
+		System.out.print("insertion texte");
+	}
 
 }
