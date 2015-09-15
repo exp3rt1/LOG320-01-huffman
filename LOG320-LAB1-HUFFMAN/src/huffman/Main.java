1403811c31025326dc9a32172043bdaba32bd0eb
SamsonJordan <samso_000@10.196.122.25>
2015-09-15 18:45:23 -0400

    Il y a un fail, mais ça devrait être ton code

--- LOG320-LAB1-HUFFMAN/src/huffman/Main.java
+++ LOG320-LAB1-HUFFMAN/src/huffman/Main.java
@@ -1,10 +1,27 @@
 package huffman;
 
 import java.io.File;
+import java.util.Scanner;
 
-public class Main {
 
-    public static void main(String[] args) {
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+    	Scanner scanner = new Scanner(System.in);
+        System.out.print("Compresser(c) ou Decompresser(d): ");
+        String option = scanner.next();
+        System.out.print("nom du fichier: ");
+        String fichier = scanner.next();
         
+        if(option.equals('c'))
+        {
+        	System.out.print("compresse");
+        }
+        else
+        {
+        	Decompression.creationArbre();
+        }
     }
 }
